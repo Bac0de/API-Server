e35417793773ce33a5bd6bd1647603a6eb0b1d82
Daeho Kim <oh3vci@gmail.com>
2017-11-30 03:12:16 +0900

    Add cors module for solving cors issue

--- Start.js
+++ Start.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 var fs = require('fs');
 var bodyParser = require('body-parser');
@@ -11,16 +12,16 @@ var path = require('path');
 ////// Start Load Models ///////
 
 sequelize = new Sequelize('vm_db', null, null, {
-	dialect:"sqlite",
-	storage:"./db.sqlite"
+	dialect: 'sqlite',
+	storage: './db.sqlite'
 });
 
 sequelize
 	.authenticate()
-	.then(function(err){
+	.then(() => {
 		console.log('Connection has been established successfully.');
-	}, function(err)
-	{
+	})
+	.catch(err => {
 		console.log('unable to connect to the database', err);
 	});
 
@@ -29,46 +30,40 @@ const db = {};
 fs.readdirSync('models/')
   .filter((file) => (file.indexOf('.') !== 0))
   .forEach((file) => {
-  
-	const model = sequelize.import(path.join('models/', file));
-	db[model.name] = model;
-
+		const model = sequelize.import(path.join('models/', file));
+		db[model.name] = model;
 });
 
 db.sequelize = sequelize;
-db.Sequelize = sequelize;
 
 module.exports = db;
 
 ////// End Load Models ///////
-
-
 var router = require('./controllers/router.js');
 
-
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(router);
-http_server = app.listen(10000, function(){});
+http_server = app.listen(10000, function () {
+	console.log('Readty');
+});
 
 var server_config = batu_http.LoadJsonSync('./config/config.json');
 
+batu_http.LoadFileJson(server_config.host_conf_dir, function (err, data) {
+  batu_config = data;
+	if (batu_config.host_type === "master") {		
+		ssdp_server = new SSDP({
+			location: require('ip').address() + ':10000/config',
+			sourcePort: 1900			    
+		});
 
-batu_http.LoadFileJson(server_config.host_conf_dir, function(err, data){
-		batu_config = data;
-		if(batu_config.host_type === "master")
-		{		
-			ssdp_server = new SSDP({
-						 location: require('ip').address() + ':10000/config',
-					   sourcePort: 1900			    
-			});
-
-			ssdp_server.addUSN('urn:schemas-upnp-org:service:ContentDirectory:1')
-
-			ssdp_server.on('advertise-alive', function (heads) { } );
-			ssdp_server.on('advertise-bye', function (heads) { } );
-			ssdp_server.start();
-			console.log("master-host start");		
-		}
+		ssdp_server.addUSN('urn:schemas-upnp-org:service:ContentDirectory:1')
+		ssdp_server.on('advertise-alive', function (heads) { } );
+		ssdp_server.on('advertise-bye', function (heads) { } );
+		ssdp_server.start();
+		console.log("master-host start");		
+	}
 });
 
